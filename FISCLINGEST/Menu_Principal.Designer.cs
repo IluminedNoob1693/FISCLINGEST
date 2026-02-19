95154b78ba4be94e3b9f4ecdfe7928530c8e9e67
jairc <jairc@JAIR>
2026-02-19 12:57:22 -0600

    Agregar archivos de proyecto.

---
+++ FISCLINGEST/Menu_Principal.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace FISCLINGEST
+{
+    partial class Menu_Principal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menu_Vertical = new System.Windows.Forms.Panel();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Usuarios = new System.Windows.Forms.Button();
+            this.btn_Citas = new System.Windows.Forms.Button();
+            this.btn_Cuotas = new System.Windows.Forms.Button();
+            this.btn_Pacientes = new System.Windows.Forms.Button();
+            this.Barra_Título = new System.Windows.Forms.Panel();
+            this.btn_Maximizar = new System.Windows.Forms.Button();
+            this.btn_minimizar = new System.Windows.Forms.Button();
+            this.btn_Cerrar = new System.Windows.Forms.Button();
+            this.btn_ContraerMenu = new System.Windows.Forms.Button();
+            this.Panel_contenedor = new System.Windows.Forms.Panel();
+            this.txt_contraseña = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.btn_IniciarSesion = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_Usuario = new System.Windows.Forms.Label();
+            this.lb_bienvenidos = new System.Windows.Forms.Label();
+            this.Menu_Vertical.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Barra_Título.SuspendLayout();
+            this.Panel_contenedor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menu_Vertical
+            // 
+            this.Menu_Vertical.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Menu_Vertical.Controls.Add(this.pictureBox5);
+            this.Menu_Vertical.Controls.Add(this.pictureBox4);
+            this.Menu_Vertical.Controls.Add(this.pictureBox3);
+            this.Menu_Vertical.Controls.Add(this.pictureBox2);
+            this.Menu_Vertical.Controls.Add(this.pictureBox1);
+            this.Menu_Vertical.Controls.Add(this.Usuarios);
+            this.Menu_Vertical.Controls.Add(this.btn_Citas);
+            this.Menu_Vertical.Controls.Add(this.btn_Cuotas);
+            this.Menu_Vertical.Controls.Add(this.btn_Pacientes);
+            this.Menu_Vertical.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Menu_Vertical.Location = new System.Drawing.Point(0, 0);
+            this.Menu_Vertical.Name = "Menu_Vertical";
+            this.Menu_Vertical.Size = new System.Drawing.Size(250, 611);
+            this.Menu_Vertical.TabIndex = 0;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(12, 218);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(37, 39);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(12, 173);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(37, 39);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 126);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(37, 39);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 39);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(232, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Usuarios
+            // 
+            this.Usuarios.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Usuarios.FlatAppearance.BorderSize = 0;
+            this.Usuarios.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.Usuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Usuarios.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Usuarios.Location = new System.Drawing.Point(0, 218);
+            this.Usuarios.Name = "Usuarios";
+            this.Usuarios.Size = new System.Drawing.Size(250, 40);
+            this.Usuarios.TabIndex = 3;
+            this.Usuarios.Text = "Uusarios";
+            this.Usuarios.UseVisualStyleBackColor = false;
+            this.Usuarios.Click += new System.EventHandler(this.Usuarios_Click);
+            // 
+            // btn_Citas
+            // 
+            this.btn_Citas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Citas.FlatAppearance.BorderSize = 0;
+            this.btn_Citas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btn_Citas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Citas.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Citas.Location = new System.Drawing.Point(0, 126);
+            this.btn_Citas.Name = "btn_Citas";
+            this.btn_Citas.Size = new System.Drawing.Size(250, 40);
+            this.btn_Citas.TabIndex = 2;
+            this.btn_Citas.Text = "Citas";
+            this.btn_Citas.UseVisualStyleBackColor = false;
+            this.btn_Citas.Click += new System.EventHandler(this.btn_Citas_Click);
+            // 
+            // btn_Cuotas
+            // 
+            this.btn_Cuotas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Cuotas.FlatAppearance.BorderSize = 0;
+            this.btn_Cuotas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btn_Cuotas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Cuotas.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Cuotas.Location = new System.Drawing.Point(0, 172);
+            this.btn_Cuotas.Name = "btn_Cuotas";
+            this.btn_Cuotas.Size = new System.Drawing.Size(250, 40);
+            this.btn_Cuotas.TabIndex = 1;
+            this.btn_Cuotas.Text = "Cuotas";
+            this.btn_Cuotas.UseVisualStyleBackColor = false;
+            this.btn_Cuotas.Click += new System.EventHandler(this.btn_Cuotas_Click);
+            // 
+            // btn_Pacientes
+            // 
+            this.btn_Pacientes.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Pacientes.FlatAppearance.BorderSize = 0;
+            this.btn_Pacientes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btn_Pacientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Pacientes.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Pacientes.Location = new System.Drawing.Point(0, 80);
+            this.btn_Pacientes.Name = "btn_Pacientes";
+            this.btn_Pacientes.Size = new System.Drawing.Size(250, 40);
+            this.btn_Pacientes.TabIndex = 0;
+            this.btn_Pacientes.Text = "Pacientes";
+            this.btn_Pacientes.UseVisualStyleBackColor = false;
+            this.btn_Pacientes.Click += new System.EventHandler(this.btn_Pacientes_Click);
+            // 
+            // Barra_Título
+            // 
+            this.Barra_Título.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Barra_Título.Controls.Add(this.btn_Maximizar);
+            this.Barra_Título.Controls.Add(this.btn_minimizar);
+            this.Barra_Título.Controls.Add(this.btn_Cerrar);
+            this.Barra_Título.Controls.Add(this.btn_ContraerMenu);
+            this.Barra_Título.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Barra_Título.Location = new System.Drawing.Point(250, 0);
+            this.Barra_Título.Name = "Barra_Título";
+            this.Barra_Título.Size = new System.Drawing.Size(1034, 45);
+            this.Barra_Título.TabIndex = 1;
+            this.Barra_Título.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Barra_Título_MouseDown);
+            // 
+            // btn_Maximizar
+            // 
+            this.btn_Maximizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Maximizar.Location = new System.Drawing.Point(980, 3);
+            this.btn_Maximizar.Name = "btn_Maximizar";
+            this.btn_Maximizar.Size = new System.Drawing.Size(18, 18);
+            this.btn_Maximizar.TabIndex = 2;
+            this.btn_Maximizar.Text = "+";
+            this.btn_Maximizar.UseVisualStyleBackColor = true;
+            this.btn_Maximizar.Click += new System.EventHandler(this.btn_Maximizar_Click);
+            // 
+            // btn_minimizar
+            // 
+            this.btn_minimizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_minimizar.Location = new System.Drawing.Point(956, 3);
+            this.btn_minimizar.Name = "btn_minimizar";
+            this.btn_minimizar.Size = new System.Drawing.Size(18, 18);
+            this.btn_minimizar.TabIndex = 3;
+            this.btn_minimizar.Text = "-";
+            this.btn_minimizar.UseVisualStyleBackColor = true;
+            this.btn_minimizar.Click += new System.EventHandler(this.btn_minimizar_Click);
+            // 
+            // btn_Cerrar
+            // 
+            this.btn_Cerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Cerrar.Location = new System.Drawing.Point(1004, 3);
+            this.btn_Cerrar.Name = "btn_Cerrar";
+            this.btn_Cerrar.Size = new System.Drawing.Size(18, 18);
+            this.btn_Cerrar.TabIndex = 1;
+            this.btn_Cerrar.Text = "X";
+            this.btn_Cerrar.UseVisualStyleBackColor = true;
+            this.btn_Cerrar.Click += new System.EventHandler(this.btn_Cerrar_Click);
+            // 
+            // btn_ContraerMenu
+            // 
+            this.btn_ContraerMenu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ContraerMenu.Location = new System.Drawing.Point(6, 3);
+            this.btn_ContraerMenu.Name = "btn_ContraerMenu";
+            this.btn_ContraerMenu.Size = new System.Drawing.Size(35, 35);
+            this.btn_ContraerMenu.TabIndex = 0;
+            this.btn_ContraerMenu.Text = "menu";
+            this.btn_ContraerMenu.UseVisualStyleBackColor = true;
+            this.btn_ContraerMenu.Click += new System.EventHandler(this.btn_Menu_Click);
+            // 
+            // Panel_contenedor
+            // 
+            this.Panel_contenedor.Controls.Add(this.txt_contraseña);
+            this.Panel_contenedor.Controls.Add(this.maskedTextBox1);
+            this.Panel_contenedor.Controls.Add(this.btn_IniciarSesion);
+            this.Panel_contenedor.Controls.Add(this.label3);
+            this.Panel_contenedor.Controls.Add(this.lb_Usuario);
+            this.Panel_contenedor.Controls.Add(this.lb_bienvenidos);
+            this.Panel_contenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Panel_contenedor.Location = new System.Drawing.Point(250, 45);
+            this.Panel_contenedor.Name = "Panel_contenedor";
+            this.Panel_contenedor.Size = new System.Drawing.Size(1034, 566);
+            this.Panel_contenedor.TabIndex = 2;
+            // 
+            // txt_contraseña
+            // 
+            this.txt_contraseña.Location = new System.Drawing.Point(409, 138);
+            this.txt_contraseña.Name = "txt_contraseña";
+            this.txt_contraseña.Size = new System.Drawing.Size(154, 20);
+            this.txt_contraseña.TabIndex = 7;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(384, 212);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(213, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            // 
+            // btn_IniciarSesion
+            // 
+            this.btn_IniciarSesion.Location = new System.Drawing.Point(431, 266);
+            this.btn_IniciarSesion.Name = "btn_IniciarSesion";
+            this.btn_IniciarSesion.Size = new System.Drawing.Size(109, 42);
+            this.btn_IniciarSesion.TabIndex = 5;
+            this.btn_IniciarSesion.Text = "Iniciar sesión";
+            this.btn_IniciarSesion.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Swis721 Hv BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(423, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Contraseña:";
+            // 
+            // lb_Usuario
+            // 
+            this.lb_Usuario.AutoSize = true;
+            this.lb_Usuario.Font = new System.Drawing.Font("Swis721 Hv BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Usuario.Location = new System.Drawing.Point(441, 96);
+            this.lb_Usuario.Name = "lb_Usuario";
+            this.lb_Usuario.Size = new System.Drawing.Size(99, 25);
+            this.lb_Usuario.TabIndex = 3;
+            this.lb_Usuario.Text = "Usuario:";
+            // 
+            // lb_bienvenidos
+            // 
+            this.lb_bienvenidos.AutoSize = true;
+            this.lb_bienvenidos.Font = new System.Drawing.Font("Swis721 Hv BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_bienvenidos.Location = new System.Drawing.Point(344, 3);
+            this.lb_bienvenidos.Name = "lb_bienvenidos";
+            this.lb_bienvenidos.Size = new System.Drawing.Size(319, 25);
+            this.lb_bienvenidos.TabIndex = 2;
+            this.lb_bienvenidos.Text = "BIENVENIDO A FISCLINGEST";
+            // 
+            // Menu_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 611);
+            this.Controls.Add(this.Panel_contenedor);
+            this.Controls.Add(this.Barra_Título);
+            this.Controls.Add(this.Menu_Vertical);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Menu_Principal";
+            this.Text = "Form1";
+            this.Menu_Vertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Barra_Título.ResumeLayout(false);
+            this.Panel_contenedor.ResumeLayout(false);
+            this.Panel_contenedor.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Menu_Vertical;
+        private System.Windows.Forms.Panel Barra_Título;
+        private System.Windows.Forms.Button Usuarios;
+        private System.Windows.Forms.Button btn_Citas;
+        private System.Windows.Forms.Button btn_Cuotas;
+        private System.Windows.Forms.Button btn_Pacientes;
+        private System.Windows.Forms.Button btn_Maximizar;
+        private System.Windows.Forms.Button btn_minimizar;
+        private System.Windows.Forms.Button btn_Cerrar;
+        private System.Windows.Forms.Button btn_ContraerMenu;
+        private System.Windows.Forms.Panel Panel_contenedor;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_Usuario;
+        private System.Windows.Forms.Label lb_bienvenidos;
+        private System.Windows.Forms.Button btn_IniciarSesion;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txt_contraseña;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ FISCLINGEST/Menu_Principal.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace FISCLINGEST
+{
+    public partial class Menu_Principal : Form  
+    {
+        public Menu_Principal()
+        {
+            InitializeComponent();
+        }
+
+        //Para contraer el panel lateral
+        private void btn_Menu_Click(object sender, EventArgs e)
+        {
+            if (Menu_Vertical.Width == 250)
+            {
+                Menu_Vertical.Width = 70;
+            }
+            else
+                Menu_Vertical.Width = 250;
+        }
+        //Botones para cerrar, minimizar, y restaurar o maximizar el programa
+        private void btn_Cerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_Maximizar_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //Para poder mover la ventana a cualquier direccion
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void Barra_Título_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        //Para poder abrir otras ventanas dentro de la principal
+        private void AbrirFormEnPanel(object Formhijo)
+        {
+            if (this.Panel_contenedor.Controls.Count > 0)
+                this.Panel_contenedor.Controls.RemoveAt(0);
+            Form fh = Formhijo as Form;
+            fh.TopLevel = false;
+            fh.Dock = DockStyle.Fill;
+            this.Panel_contenedor.Controls.Add(fh);
+            this.Panel_contenedor.Tag = fh;
+            fh.Show();
+        }
+
+        private void btn_Pacientes_Click(object sender, EventArgs e)
+        {
+            AbrirFormEnPanel(new Pacientes());
+        }
+
+        private void btn_Citas_Click(object sender, EventArgs e)
+        {
+            AbrirFormEnPanel(new Citas());
+        }
+
+        private void btn_Cuotas_Click(object sender, EventArgs e)
+        {
+            AbrirFormEnPanel(new Cuotas());
+        }
+
+        private void Usuarios_Click(object sender, EventArgs e)
+        {
+            AbrirFormEnPanel(new Usuarios());
+        }
+    }
+}
